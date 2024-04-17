d258d5674a87ba89a1165db3134f82e752519390
Stein0705 <schuesslerpierre@gmail.com>
2024-04-17 19:45:36 +0200

    Made it stop after not finding anything for 30s

--- PredictionConsommation.py
+++ PredictionConsommation.py
@@ -12,8 +12,6 @@ def Predict(genome, entres):
         #print(f"consommation : {consommation}")
         return consommation
 
-
-
 def Read_Data():
     with open("data.csv", newline='') as file:
         dict_reader = csv.DictReader(file, delimiter=';')
@@ -37,13 +35,10 @@ def Read_Data():
         new_array = numpy.array(new_data)
         for y in range(len(new_array)):
                for x in range(11):
-                      new_array[y][x] = float(new_array[y][x]) + 5
+                      new_array[y][x] = float(new_array[y][x]) + 10
 
         
         return new_array.astype(float)
-
-
-
 
 def Test(genome, data):
         score = 0.0
@@ -52,103 +47,94 @@ def Test(genome, data):
                 score += abs(Predict(genome, data[i])- float(data[i][10]))
         return score
 
-
-def Select_Best(option1, option2):
+def Select_Best(option1, option2, depth):
         global generation
+        global Last_generation
         score_model = Test(option1, data)
         score_new_model = Test(option2, data)
         
         if score_model > score_new_model:
-                print(score_model / 2982 , score_new_model/ 2982, option2)
                 average_deviation_generations.append(score_new_model / 2982)
-                print(f'Generation {generation} / Average Deviation: {Test(model, data)/2982} / Model {model}')
+                print(f'\033[32m Generation {generation + 1} \033[39m / Average Deviation: {score_new_model/2982} / Model {model} / depth = {depth}')
+                Last_generation = time.time()
                 return True
         else:
-                print(f"Generation {generation}; Average Deviation {score_model/2982}, model {option1}, no evolution")
+                print(f'\033[31m Generation {generation + 1} \033[39m / Average Deviation: {Test(model, data)/2982} / Model {model} / depth = {depth} / Last Generation: {round(abs(Last_generation - time.time()))}')
                 return False
 
 model = numpy.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
-generation = 1
-change = 1
-def Scan():
-        global model
-        global generation
-        global change
-        generation += 1
-        
-        for i in range(10):
-                new_model = model.copy()
-                new_model[i] += change
-                
-                if Select_Best(model, new_model) == True:
-                        
-                        model = new_model.copy()
-                        
-                        if generation < 1000:
-                                        
-                                        Scan()
-                                        return
-                new_model = model.copy()
-                new_model[i] -= change
-                
-                if Select_Best(model, new_model) == True:
-                        
-                        model = new_model.copy()
-                        
-                        if generation < 1000:
-                                Scan()
-                                return
-
-def Scan2D():
-        global model
-        global generation
-        global change
-        generation += 1
-        for x in range(10):
-                for y in range(10):
-                        new_model = model.copy()
-                        new_model[x] += change
-                        new_model[y] += change
-                        if Select_Best(model, new_model) == True:
-                        
-                                model = new_model.copy()
-                        
-                                if generation < 1000:
-                                        Scan2D()
-                                        return
-                        new_model = model.copy()
-                        new_model[x] += change
-                        new_model[y] -= change
-                        if Select_Best(model, new_model) == True:
-                        
-                                model = new_model.copy()
-                        
-                                if generation < 1000:
-                                        Scan2D()
-                                        return
-                        new_model = model.copy()
-                        new_model[x] -= change
-                        new_model[y] -= change
-                        if Select_Best(model, new_model) == True:
-                        
-                                model = new_model.copy()
-                        
-                                if generation < 1000:
-                                        Scan2D()
-                                        return
-
-                        
-average_deviation_generations = []
-data = Read_Data()
-model = numpy.array([-24.0, 54.0, 64.0, 67.0, 63.0, 58.0, -6.0, -86.0, -35.0, -11.0])
 change = 1
 generation = 0
+Last_generation = time.time()
 
-Scan()
-Scan2D()
+from itertools import product
+
+def Scan(depth, lastchange):
+        global model
+        global change
+        global generation
+        global Last_generation
+        generation += 1
+        
+        
+        if lastchange != None:
+                new_model = model.copy()
+
+                # Apply changes
+                for change_value in lastchange:
+                        index = change_value - 10 if change_value > 10 else change_value
+                        value = change if change_value <= 10 else -change
+                        
+                        if index < len(new_model):
+                                new_model[index] += value
+                                
+                
+                # Check if new model is better
+                if Select_Best(model, new_model , depth):
+                        model = new_model.copy()
+                        if Last_generation - time.time() < 30:
+                                Scan(depth, lastchange)
+                        return
+                
+        for changes in product(range(21), repeat=depth):
+                new_model = model.copy()
+
+                # Apply changes
+                for change_value in changes:
+                        index = change_value - 10 if change_value > 10 else change_value
+                        value = change if change_value <= 10 else -change
+                        
+                        if index < len(new_model):
+                                new_model[index] += value
+                
+                
+                # Check if new model is better
+                if Select_Best(model, new_model , depth):
+                        
+                        model = new_model.copy()
+                        
+                        if abs(Last_generation - time.time()) < 30:
+                                Scan(depth, changes)
+                        
+                        return
+                
+        
+        if abs(Last_generation - time.time()) < 30:
+                Scan(depth+1, None)
+                print("The code is behind the second scan")
+        
+        return
+    
+
+
+average_deviation_generations = []
+data = Read_Data()
+
+
+
 
 print(model)
-
+Scan(1, None)
 
 
 plt.plot(average_deviation_generations, 'r')
